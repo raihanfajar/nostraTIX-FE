732bc74e49788a67043f403dc67ecfaf25d8b7e6
raihanfajar <rhnfjr@gmail.com>
2025-08-01 23:28:26 +0700

    fix: useSearch params by suspense

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,14 +1,18 @@
-import EventList from '@/components/EventList'
-import SearchBar from '@/components/SearchBar/SearchBar'
-import React from 'react'
+import EventList from "@/components/EventList";
+import SearchBar from "@/components/SearchBar/SearchBar";
+import React, { Suspense } from "react";
 
 function eventPage() {
   return (
-    <section className='container px-4 mx-auto'>
-        <SearchBar/>
-        <EventList/>
+    <section className="container mx-auto px-4">
+      <Suspense>
+        <SearchBar />
+      </Suspense>
+      <Suspense>
+        <EventList />
+      </Suspense>
     </section>
-)
+  );
 }
 
-export default eventPage
+export default eventPage;
